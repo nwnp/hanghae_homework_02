415d748481536b89e9f8677ce0a3be58d7a539aa
nwnp <jjw_0418@naver.com>
2022-06-11 09:06:22 +0900

    fix: statuscode 수정

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const register = async (req, res, next) => {
   try {
     if (res.locals.user) {
-      return res.status(404).json({
+      return res.status(400).json({
         result: {
           success: false,
           errorMessage: "이미 로그인한 상태입니다.",
@@ -17,7 +17,7 @@ const register = async (req, res, next) => {
 
     // password check
     if (password !== passwordCheck) {
-      return res.status(400).json({
+      return res.status(401).json({
         result: {
           success: false,
           errorMessage: "비밀번호가 맞지 않습니다.",
@@ -27,7 +27,7 @@ const register = async (req, res, next) => {
 
     // password에 nickname값이 있는지 체크
     if (password.includes(nickname)) {
-      return res.status(400).json({
+      return res.status(401).json({
         result: {
           success: false,
           errorMessage: "닉네임이 들어간 패스워드는 사용할 수 없습니다.",
@@ -38,7 +38,7 @@ const register = async (req, res, next) => {
     // email 중복체크
     const existEmail = await User.findOne({ where: { email } });
     if (existEmail) {
-      return res.status(400).json({
+      return res.status(401).json({
         result: {
           success: false,
           errorMessage: "이미 존재하는 회원입니다.",
@@ -49,7 +49,7 @@ const register = async (req, res, next) => {
     // nickname 중복체크
     const existNickname = await User.findOne({ where: { nickname } });
     if (existNickname) {
-      return res.status(400).json({
+      return res.status(401).json({
         result: {
           success: false,
           errorMessage: "이미 존재하는 닉네임입니다.",
@@ -90,7 +90,7 @@ const login = async (req, res, next) => {
     // email check
     const exUser = await User.findOne({ where: { email } });
     if (!exUser) {
-      return res.status(400).json({
+      return res.status(401).json({
         result: {
           success: false,
           errorMessage: "이메일 또는 패스워드가 틀렸습니다.",
@@ -117,7 +117,7 @@ const login = async (req, res, next) => {
         },
       });
     } else {
-      return res.status(400).json({
+      return res.status(401).json({
         result: {
           success: false,
           errorMessage: "이메일 또는 패스워드가 틀렸습니다.",
